34438f3d8852a03c1f69fc506caea0586776cc7b
Harrypoggers25 <mfa.g4e@gmail.com>
2026-07-27 22:19:35 +0800

    Update: Abstract book extract function

--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -1,8 +1,13 @@
+// CONFIGS
+import { db, WordBuffer, WordType } from "../configs/db.config";
+
+// MODULES
+import * as cheerio from "cheerio";
+import ch from "@harrypoggers25/color-utils";
 import fs from "fs/promises";
 import JSZip from "jszip";
-import * as cheerio from "cheerio";
 import kuromoji from "kuromoji";
-import ch from "@harrypoggers25/color-utils";
+import Message from "@harrypoggers25/message";
 
 export type PosType = '助動詞' | '助詞' | '記号' | 'フィラー' | 'その他' | '接続詞' | '連体詞' | '名詞' | '動詞' | '副詞' | '接頭詞' | '形容詞' | '感動詞';
 
@@ -72,7 +77,7 @@ export function parseToken(token: IToken): IParsedToken {
 	return { token_id, wt_name, w_basic_form, w_reading, w_pos_details, wt_description, surface_form };
 }
 
-export async function extractEpub(path: string): Promise<IBook> {
+export async function extractEpubFile(path: string): Promise<IBook> {
 	const file = await fs.readFile(path);
 	const zip = await JSZip.loadAsync(file);
 
@@ -118,6 +123,73 @@ export async function parseSentence(sentence: string, filterOut: Array<string> =
 	return tokenizer.tokenize(sentence).filter(elem => !filterOut.includes(elem.pos)) as Array<IToken>;
 }
 
-export function displayProgress(i: number, count: number) {
+function displayProgress(i: number, count: number) {
 	console.log(ch.green('Progress:'), `${Math.round((i / count * 100) * 100) / 100}%`);
+}
+
+type ExtractBookOptions = { filteredPos?: Array<PosType>, showDuplicate?: boolean };
+export async function extractBook(fileName: string, options?: ExtractBookOptions) {
+	const filteredPos = options?.filteredPos ?? [];
+	const showDuplicate = options?.showDuplicate ?? true;
+	const ignores: Array<number> = [];
+
+	try {
+		const book = await extractEpubFile(fileName);
+		const { count, parsedBook } = parseBook(book);
+
+		let i = 0;
+		for (const { sentences } of parsedBook) {
+			for (const sentence of sentences) {
+				const parsedSentence = (await parseSentence(sentence, filteredPos)).map(token => parseToken(token));
+				for (const { token_id, wt_name, w_basic_form, w_reading, w_pos_details, wt_description, surface_form } of parsedSentence) {
+					const created_at = new Date();
+					const transaction = await db.transaction({ rollbackOnError: true });
+
+					if (!w_reading) {
+						if (!ignores.includes(token_id)) {
+							console.log(ch.cyan('NO READING:'), { token_id, surface_form, wt_name });
+							ignores.push(token_id);
+						}
+						await transaction.commit();
+						continue;
+					}
+
+					const wordTypes = await WordType.find({ where: { wt_name }, transaction });
+					if (!wordTypes) throw new Error(Message.failed(['find', 'word types', { wt_name }]));
+					if (!wordTypes.length) {
+						const newWordType = await WordType.create({ wt_name, wt_description, created_at }, { transaction });
+						if (!newWordType) throw new Error(Message.failed(['create', 'new word type', { wt_name }]));
+					}
+
+					const words = await WordBuffer.find({ where: { token_id }, transaction });
+					if (!words) throw new Error(Message.failed(['find', 'words', { token_id, w_basic_form }]));
+					const word = await (async () => {
+						if (words.length) {
+							if (!ignores.includes(token_id)) {
+								if (showDuplicate) console.log(ch.yellow('DUPLICATE ENTRY:'), { token_id, w_basic_form, w_reading, wt_name });
+								ignores.push(token_id);
+							}
+							return words[0];
+						}
+
+						const newWord = await WordBuffer.create({ token_id, w_basic_form, w_reading, w_pos_details, created_at, wt_name }, { transaction });
+						if (!newWord) throw new Error(Message.failed(['create', 'new word', { token_id, w_basic_form }]));
+
+						return newWord;
+					})();
+
+					const updateWord = await WordBuffer.updateByPk(token_id, { count: word.count + 1 }, { transaction });
+					if (!updateWord) throw new Error(Message.failed(['update', 'word count', { token_id, w_basic_form }]));
+
+					await transaction.commit();
+				}
+				displayProgress(i + 1, count);
+				i += 1;
+			}
+		}
+
+		console.log(ch.green('SUCCESS:'), `extracted words from book ${fileName}`);
+	} catch (error: any) {
+		console.log(ch.red('EXTRACTION ERROR:'), error.message ?? error);
+	}
 }
