42d35dca31728fc51b0af363f06d2b8075f730fc
Harrypoggers25 <mfa.g4e@gmail.com>
2026-08-01 14:12:16 +0800

    Update: Db script load backup file

--- src/scripts/db.script.ts
+++ src/scripts/db.script.ts
@@ -2,9 +2,17 @@
 import ch from "@harrypoggers25/color-utils";
 import { db } from "../configs/db.config";
 
-db.sync({
-    alter: true,
-    onSuccessAlter: async (transaction) => {
-        console.log(ch.green('CREATE SCRIPT:'), `Altered db. All previous data have been`, ch.red('deleted'));
-    }
-})
+(async () => {
+	try {
+		const sync = await db.sync({ alter: true, });
+		if (!sync) throw new Error('Failed to alter sync db');
+
+		console.log(ch.green('DB SCRIPT:'), `Altered db. All previous data have been`, ch.red('deleted'));
+		const loadBackup = await db.loadBackup('./database/epub-extractor-v1.json');
+		if (!loadBackup) throw new Error('Failed to load backup');
+
+		console.log(ch.green('DB SCRIPT:'), `Load backup file`, ch.green('successful'));
+	} catch (error: any) {
+		console.log(ch.red('DB SCRIPT ERROR:'), error.message ?? error);
+	}
+})()
