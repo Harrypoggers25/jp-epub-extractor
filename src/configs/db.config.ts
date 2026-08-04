d1f90cc1ac633540bd9dbc5719f199b2577140bb
Harrypoggers25 <mfa.g4e@gmail.com>
2026-08-04 22:57:32 +0800

    Update: Sense state model

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -61,7 +61,10 @@ export interface ICleanedBuffer extends ReturnType<typeof CleanedBuffer.getEmpty
 
 export const SenseState = db.define('sense_states', {
 	ss_key: { type: DataTypes.VARCHAR(1023), allowNull: false, primaryKey: true },
-	state: { type: DataTypes.TEXT, allowNull: false },
+	state: { type: DataTypes.TEXT, allowNull: false, defaultValue: '[]' },
+	unimportant: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+	ignore: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+	merged_with: { type: DataTypes.VARCHAR(1023), allowNull: true },
 });
 export interface ISenseState extends ReturnType<typeof CleanedBuffer.getEmptyModel> { };
 
