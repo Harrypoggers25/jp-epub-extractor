f1b8e81c5b84e7d98fee92302b90dfa9b2d30fe4
Darwish Haikal <116283726+haikal143@users.noreply.github.com>
2026-08-21 13:41:36 +0800

    Add: Word and unsure page shells

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -30,4 +30,12 @@ router.get('/review', (_, res) => {
 	res.render('review');
 });
 
+router.get('/word', (_, res) => {
+	res.render('word');
+});
+
+router.get('/review/unsure', (_, res) => {
+	res.render('unsure-review');
+});
+
 export default router;
